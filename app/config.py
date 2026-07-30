63c8815c3ab664430711ea784d0460ba8d941d86
NoroiMusha37 <vo.melnyk37@gmail.com>
2026-07-31 00:38:50 +0300

    [add] security functions

--- app/config.py
+++ app/config.py
@@ -7,6 +7,9 @@ ROOT_DIR = Path(__file__).resolve().parent.parent
 
 class Settings(BaseSettings):
     DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     model_config = SettingsConfigDict(
         env_file=ROOT_DIR / ".env",
